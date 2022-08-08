b28cbb01de4effb06b3b34b743b9d0a410120faa
Badiazamane Benarab <benarabbadi05@gmail.com>
2022-08-08 10:39:26 +0200

    Adding scroll bar effects

---
+++ interactions.js
@@ -0,0 +1,11 @@
+window.addEventListener("scroll", function (event) {
+  var scroll = this.scrollY;
+  if (scroll > 0) {
+    document.getElementById("nav-header").style.backgroundColor = "white";
+    document.getElementById("nav-header").style.boxShadow =
+      "1px 1px 25px black";
+  } else {
+    document.getElementById("nav-header").style.backgroundColor = "#fffaeb";
+    document.getElementById("nav-header").style.boxShadow = "none";
+  }
+});
