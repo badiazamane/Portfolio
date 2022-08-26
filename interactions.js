0399aaaac3e5d8afa8a7260457e2f2b3ee6b322e
Badiazamane Benarab <benarabbadi05@gmail.com>
2022-08-26 19:29:51 +0200

    After adding contact

--- interactions.js
+++ interactions.js
@@ -9,3 +9,26 @@ window.addEventListener("scroll", function (event) {
     document.getElementById("nav-header").style.boxShadow = "none";
   }
 });
+document.getElementById("about1").addEventListener("click", () => {
+  window.setTimeout(() => {
+    window.scrollTo(window.scrollX, window.scrollY - 110);
+  }, 0);
+});
+document.getElementById("projects").addEventListener("click", () => {
+  console.log("Third page clicked");
+  window.setTimeout(() => {
+    window.scrollTo(window.scrollX, window.scrollY - 120);
+  }, 0);
+});
+document.getElementById("contact").addEventListener("click", () => {
+  console.log("Third page clicked");
+  window.setTimeout(() => {
+    window.scrollTo(window.scrollX, window.scrollY - 120);
+  }, 0);
+});
+document.getElementById("contact1").addEventListener("click", () => {
+  console.log("Third page clicked");
+  window.setTimeout(() => {
+    window.scrollTo(window.scrollX, window.scrollY - 120);
+  }, 0);
+});
